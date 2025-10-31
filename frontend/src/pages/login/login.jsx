cddbefe9300ee3293d3777fd2b439f9ef86800bf
EDUARDO <cheligaedu@gmail.com>
2025-10-31 11:55:30 -0300

    começando a fazer a tela de login

---
+++ frontend/src/pages/login/login.jsx
@@ -0,0 +1,13 @@
+import styles from "./login.module.css"
+export default function Login(){
+    return(
+        <div className="container">
+            <div className="pt1">
+                <h1>Login</h1>
+            </div>
+            <div className="pt2">
+                <h2>Uma plataforma de Quiz sobre a gincana</h2>
+            </div>
+        </div>
+    )
+}
