784d2619c786438a266ede296334f3d21461f9ff
marceloAst <marcelo.2007@alunos.utfpr.edu.br>
2025-11-03 16:43:17 -0300

    Seleção de Mundos atualizada

--- frontend/src/pages/chooseWorlds/chooseWorlds.jsx
+++ frontend/src/pages/chooseWorlds/chooseWorlds.jsx
@@ -1,5 +1,6 @@
 import styles from "./chooseWorlds.module.css";
 import TitleBox from "../../components/TitleBox/TitleBox";
+import StyleSquare from "../../components/StyleSquare/StyleSquare";
 import ChooseWorlds from "../../components/ChooseWorlds/ChooseWorlds";
 
 
@@ -8,7 +9,7 @@ export default function chooseWorlds(){
 
     return (
     <div className={styles.chooseWorlds}>
-
+      <StyleSquare option={2} innerColor={'var(--gray)'} outColor={'var(--black)'} position={'bottomRight'}></StyleSquare>
       <main className={styles.main}>
         <div className={styles.boxWorlds}>
             <div className={styles.Worlds}>
