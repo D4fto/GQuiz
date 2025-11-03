7fb0b7da2e36c2f49815809f0537138fa5e289e1
D4fto <163424876+D4fto@users.noreply.github.com>
2025-11-03 08:54:36 -0300

    Fazendo o componente styleSquare
    Fiz o componente que vai ser utilizado para fazer os detalhes que ficam nos cantos do figma

---
+++ frontend/src/components/StyleSquare/StyleSquare.jsx
@@ -0,0 +1,34 @@
+import styles from './StyleSquare.module.css'
+import { useRef, useEffect } from 'react'
+const stylesSquarePath = '../../assets/stylesSquares/'
+
+import Option1 from '../../assets/stylesSquares/option1.svg?react'
+import Option2 from '../../assets/stylesSquares/option2.svg?react'
+import Option3 from '../../assets/stylesSquares/option3.svg?react'
+import Option4 from '../../assets/stylesSquares/option4.svg?react'
+
+const options = {
+  1: Option1,
+  2: Option2,
+  3: Option3,
+  4: Option4,
+}
+
+export default function StyleSquare({option, position, innerColor, outColor}){
+  const svgRef = useRef(null)
+  const SvgComponent = options[option]
+
+  useEffect(()=>{
+    if (svgRef.current) {
+      const inner = svgRef.current.getElementById('innerStyleSquare');
+      const outer = svgRef.current.getElementById('outStyleSquare');
+      if (inner) inner.setAttribute('fill', innerColor);
+      if (outer) outer.setAttribute('fill', outColor);
+    }
+  }, [innerColor, outColor])
+
+
+  return <div className={`${styles.styleSquare} ${styles[position]}`}>
+    <SvgComponent ref={svgRef}></SvgComponent>
+  </div>
+}
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,24 +1,32 @@
-import styles from './Home.module.css';
-import TitleBox from '../../components/TitleBox/TitleBox';
-import MainButton from '../../components/MainButton/MainButton';
-import { useNavigate } from 'react-router-dom';
+import styles from "./Home.module.css";
+import TitleBox from "../../components/TitleBox/TitleBox";
+import MainButton from "../../components/MainButton/MainButton";
+import StyleSquare from "../../components/StyleSquare/StyleSquare";
+import { useNavigate } from "react-router-dom";
 
 export default function Home() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   return (
     <div className={styles.home}>
+      <StyleSquare option={2} innerColor={'var(--gray)'} outColor={'var(--black)'} position={'bottomRight'}></StyleSquare>
       <div className={styles.rank}>
         <TitleBox title="Ranking Geral" />
-
       </div>
       <div className={styles.mainContent}>
-        <p className={styles.description}>Será que você tem capacidade de acertar as perguntas sobre a Gincana da UTFPR 2025 ? </p>
+        <p className={styles.description}>
+          Será que você tem capacidade de acertar as perguntas sobre a Gincana
+          da UTFPR 2025 ?{" "}
+        </p>
         <div className={styles.options}>
           <MainButton text="Entrar em Sala" onClick={() => {}} />
           <MainButton text="Criar Sala" onClick={() => {}} />
-          <MainButton text="Jogar Fases" onClick={() => { navigate('/worlds') }} />
+          <MainButton
+            text="Jogar Fases"
+            onClick={() => {
+              navigate("/worlds");
+            }}
+          />
           <MainButton text="Quiz Aleatório" onClick={() => {}} />
-
         </div>
       </div>
     </div>
