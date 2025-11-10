471f83d2f7d6e0769e8ddbd670b904e617427ac4
D4fto <163424876+D4fto@users.noreply.github.com>
2025-11-10 08:39:17 -0300

    Comunicação entre a página de login e o backend

--- frontend/src/pages/login/login.jsx
+++ frontend/src/pages/login/login.jsx
@@ -4,8 +4,46 @@ import MainButton from "../../components/MainButton/MainButton";
 import StyleSquare from "../../components/StyleSquare/StyleSquare";
 import BlackQ from "../../components/BlackQ/BlackQ";
 import { Link } from 'react-router-dom';
+import { toast } from 'react-hot-toast'
+
+
 export default function Login(){
     const navigate = useNavigate()
+    function handleSubmit(event){
+        event.preventDefault()
+        fetch(import.meta.env.VITE_API_URL+'/login', {
+            method:'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            credentials: 'include',
+            body: JSON.stringify({
+                email: event.target.email.value,
+                password: event.target.senha.value
+            })
+        }).then(async (response)=>{
+            const data = await response.json();
+            if(data.error){
+                console.error(data.error)
+                toast.error(data.error)
+                return
+            }
+            navigate('/')
+        }).catch((error)=>{
+            console.error(error)
+            toast.error(error)
+        })
+    }
+    function logout(event){
+        event.preventDefault()
+        fetch(import.meta.env.VITE_API_URL+'/login/logout', {
+            method: 'POST',
+            credentials: 'include'
+        }).then(async (response) => {
+            const data = await response.json();
+            console.log(data);
+        })
+    }
 
 
     return(
@@ -13,7 +51,7 @@ export default function Login(){
             <div className={styles.ptU}>
                 <StyleSquare option={2} innerColor={'var(--primary-dark)'} outColor={'var(--primary)'} position={'bottomRight'}></StyleSquare>
                 <StyleSquare option={3} innerColor={'var(--primary-dark)'} position={'topLeft'}></StyleSquare>
-                <div className={styles.login}>
+                <form className={styles.login} onSubmit={handleSubmit}>
                     <div className={styles.title}>LOGIN</div>
                     <div className={styles.else}>
                         <div className={styles.validation}>
@@ -23,16 +61,16 @@ export default function Login(){
                             <input className={styles.input} type="text" id="senha" name="senha" placeholder="*******"/>
                         </div>
                         
-                        <MainButton text="Fazer Login" onClick={() => {navigate("/")}} />
+                        <MainButton text="Fazer Login" onClick={() => {}} />
                         
                         <div className={styles.account}>Não tem conta? <Link className={styles.link} to={'/create-account'}>Cadastre-se</Link></div>
                         <div className={styles.white}></div>
-                        <div className={styles.google}>
+                        <div className={styles.google} onClick={logout}>
                             <img className={styles.img} src="https://img.icons8.com/?size=100&id=V5cGWnc9R4xj&format=png&color=000000" alt="" />
                             continuar com google
                         </div>
                     </div>  
-                </div>
+                </form>
             </div>
                 <BlackQ/>
         </div>
