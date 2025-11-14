3d0b1e99b50febb3d2df03fd9e04af9cd167773b
Eduardo <cheligaedu@gmail.com>
2025-11-14 11:56:09 -0300

    Create account verification
    Creating communication between create account page and backend

--- frontend/src/pages/CreateAccount/CreateAccount.jsx
+++ frontend/src/pages/CreateAccount/CreateAccount.jsx
@@ -2,10 +2,45 @@ import styles from "./CreateAccount.module.css"
 import { useNavigate } from "react-router-dom";
 import MainButton from "../../components/MainButton/MainButton";
 import StyleSquare from "../../components/StyleSquare/StyleSquare";
+import { toast } from 'react-hot-toast'
 import BlackQ from "../../components/BlackQ/BlackQ";
 import { Link } from 'react-router-dom';
 export default function CreateAccount(){
     const navigate = useNavigate()
+    function handleSubmit(event){
+        event.preventDefault()
+        fetch(import.meta.env.VITE_API_URL+'/login/create-account', {
+            method:'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            credentials: 'include',
+            body: JSON.stringify({
+                email: event.target.email.value,
+                password: event.target.senha.value,
+                username: event.target.user.value
+            })
+        }).then(async (response) => {
+            const data = await response.json();
+        
+            if (!response.ok) {
+                if (data.error.code === "P2002") {
+                    console.error(data.error.meta.target[0]+" já existe");
+                    toast.error(data.error.meta.target[0]+" já existe")
+                    return;
+                }
+        
+                console.error("Erro desconhecido:", data.error);
+                return;
+            }
+        
+            navigate('/login');
+        })
+        .catch((error) => {
+            console.error("Erro de rede ou fetch:", error);
+            // toast.error("Erro de conexão")
+        });
+    }
 
 
     return(
@@ -13,7 +48,7 @@ export default function CreateAccount(){
             <div className={styles.ptU}>
                 <StyleSquare option={2} innerColor={'var(--primary-dark)'} outColor={'var(--primary)'} position={'bottomRight'}></StyleSquare>
                 <StyleSquare option={3} innerColor={'var(--primary-dark)'} position={'topLeft'}></StyleSquare>
-                <div className={styles.login}>
+                <form className={styles.login} onSubmit={handleSubmit}>
                     <div className={styles.title}>CADASTRAR-SE</div>
                     <div className={styles.else}>
                         <div className={styles.validation}>
@@ -25,10 +60,10 @@ export default function CreateAccount(){
                             <input className={styles.input} type="text" id="senha" name="senha" placeholder="*******"/>
                         </div>
                         
-                        <MainButton text="Fazer Cadastro" onClick={() => {navigate("/")}} />
+                        <MainButton text="Fazer Cadastro" type="submit" />
                         
                     </div>  
-                </div>
+                </form>
             </div>
                 <BlackQ/>
         </div>
