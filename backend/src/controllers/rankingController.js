a61eecc0ecf76bdde52eaf24ee64093690968e0f
D4fto <163424876+D4fto@users.noreply.github.com>
2025-11-17 10:11:15 -0300

    Criando a rota para get em ranking

--- backend/src/controllers/rankingController.js
+++ backend/src/controllers/rankingController.js
@@ -1,5 +1,25 @@
+import prisma from "../config/db.js"
 
 
-export function getRanking(req, res){
-  res.send({message: "ranking"})
+export async function getRanking(req, res){
+  const limit = req.params.limit || 10
+  try{
+    const response = await prisma.user.findMany({
+      take: limit,
+      where: {
+        isAdmin: false
+      },
+      orderBy: {
+        points: 'desc'
+      },
+      select:{
+        username: true,
+        points: true,
+        id_userImg: true
+      }
+    })
+    res.send({response})
+  }catch(e){
+    res.status(400).send({error: e})
+  }
 }
