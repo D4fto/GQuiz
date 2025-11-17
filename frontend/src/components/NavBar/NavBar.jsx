3efa4a7a4265f32cc151f6372515a21516e01b6c
D4fto <163424876+D4fto@users.noreply.github.com>
2025-11-17 08:14:58 -0300

    Filtragem de onde aparece a opção de ver perfil

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -1,24 +1,32 @@
 import styles from './NavBar.module.css';
 import gql from '../../assets/gquiz-logo-white.svg';
 import perfil from '../../assets/perfil.svg';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
+
+const noProfileRoutes = ['/question']
 
 export default function NavBar() {
-    return (
-        <nav className={styles.navbar}>
-            <div className={styles.logo}>
-                <img src={gql} alt="GQuiz Logo" className={styles.logoImage} />
-            </div>
-            <div className={styles.routes}>
-              <Link to={'/'}>Home</Link>
-              <Link to={'/login'}>Login</Link>
-              <Link to={'/worlds'}>Worlds</Link>
-            </div>
+  const location = useLocation()
+
+
+  return (
+      <nav className={styles.navbar}>
+          <div className={styles.logo}>
+              <img src={gql} alt="GQuiz Logo" className={styles.logoImage} />
+          </div>
+          <div className={styles.routes}>
+            <Link to={'/'}>Home</Link>
+            <Link to={'/login'}>Login</Link>
+            <Link to={'/worlds'}>Worlds</Link>
+            <Link to={'/question'}>Question</Link>
+          </div>
+          {
+            !noProfileRoutes.includes(location.pathname) &&
             <div className={styles.user}>
               Ver Perfil
               <img src={perfil} alt="Foto de perfil" className={styles.userImage}/>
-
             </div>
-        </nav>
-    );
+          }
+      </nav>
+  );
 }
