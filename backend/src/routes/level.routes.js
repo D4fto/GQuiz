8dba715db544c189ec68291d5ef49c4718aa45a0
D4fto <163424876+D4fto@users.noreply.github.com>
2025-11-24 08:05:52 -0300

    Criação da rota de createLevel

--- backend/src/routes/level.routes.js
+++ backend/src/routes/level.routes.js
@@ -9,6 +9,7 @@ router.get("/questions/:level", authMiddleware, levelController.getLevelQuestion
 router.post("/questions/:level", authMiddleware, adminMiddleware, levelController.addQuestionsToLevel);
 router.delete("/questions/:level", authMiddleware, adminMiddleware, levelController.removeQuestionsFromLevel);
 router.get("/", authMiddleware, adminMiddleware, levelController.getLevels);
+router.post("/", authMiddleware, adminMiddleware, levelController.createLevel);
 router.patch("/:id", authMiddleware, adminMiddleware, levelController.updateLevel);
 router.delete("/:id", authMiddleware, adminMiddleware, levelController.deleteLevel);
 
