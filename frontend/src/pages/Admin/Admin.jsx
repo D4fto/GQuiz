6d9bc8c378273c57a77e3399d5eb7742688ab07a
D4fto <163424876+D4fto@users.noreply.github.com>
2025-11-24 08:06:58 -0300

    Criação da pagina de admin

---
+++ frontend/src/pages/Admin/Admin.jsx
@@ -0,0 +1,51 @@
+import styles from "./Admin.module.css";
+import { useState } from "react";
+import { CRUDCategory, CRUDWorld, CRUDQuestion, CRUDLevelHasQuestion, CRUDLevel, CRUDUser, CRUDUserHasLevel } from "../../components/CRUDS/CRUDS";
+
+export default function Admin() {
+  const [adminState, setAdminState] = useState("categorias");
+  
+  
+  
+
+  const cruds = {
+    categorias: <CRUDCategory></CRUDCategory>,
+    mundos: <CRUDWorld></CRUDWorld>,
+    levels: <CRUDLevel></CRUDLevel>,
+    users: <CRUDUser></CRUDUser>,
+    questoes:
+      <>
+        <p>Utilize a coluna <strong>option</strong> para editar e as conlunas <strong>corrects</strong> e <strong>wrongs</strong> para criar</p>
+        <CRUDQuestion></CRUDQuestion>
+      </>,
+    "fases - questoes": <CRUDLevelHasQuestion></CRUDLevelHasQuestion>,
+    "usuarios - fases": <CRUDUserHasLevel></CRUDUserHasLevel>,
+  };
+
+
+  return (
+    <div className={styles.admin}>
+      <h1>Admin</h1>
+      <div className={styles.main}>
+        <fieldset className={styles.crudSelection}>
+          <legend>Escolha o que manter</legend>
+          {Object.keys(cruds).map((key) => {
+            return (
+              <div>
+                <input
+                  type="radio"
+                  name="crud"
+                  value={key}
+                  id={key}
+                  onClick={() => setAdminState(key)}
+                />
+                <label htmlFor={key}>{key}</label>
+              </div>
+            );
+          })}
+        </fieldset>
+        <div className={styles.crud}>{cruds[adminState]}</div>
+      </div>
+    </div>
+  );
+}
