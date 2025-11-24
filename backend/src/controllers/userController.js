ae6c7e42444d74b3f4893ce558866ef4a6f41bc4
D4fto <163424876+D4fto@users.noreply.github.com>
2025-11-24 08:05:07 -0300

    Criação de funções relacionadas ao user

---
+++ backend/src/controllers/userController.js
@@ -0,0 +1,168 @@
+import prisma from "../config/db.js"
+
+export async function getMyUser(req, res) {
+  const id = req.user.id
+  try{
+    const response = await prisma.user.findUnique({
+      where: {
+        id: id
+      },
+      
+      select: {
+        email: true,
+        username: true,
+        points: true,
+        userImgs: {
+          select:{
+            imgName: true
+          }
+        }
+      }
+    })
+    res.status(200).send({data: response})
+  } catch (e) {
+    res.status(400).send({error: e})
+  }
+}
+export async function getUsers(req, res) {
+  try{
+    const response = await prisma.user.findMany({
+      
+      
+      select: {
+        id: true,
+        email: true,
+        username: true,
+        password: true,
+        points: true,
+        isAdmin: true,
+        id_userImg: true
+      }
+    })
+    res.status(200).send({data: response})
+  } catch (e) {
+    res.status(400).send({error: e})
+  }
+}
+
+export async function createUser(req, res) {
+  const { data } = req.body
+  try{
+    console.log(data)
+    const response = await prisma.user.create({
+      data: data
+    })
+    res.status(200).send({data: response})
+  } catch (e) {
+    res.status(400).send({error: e})
+  }
+}
+
+export async function deleteUser(req, res) {
+  const id = parseInt(req.params.id)
+  try{
+    await prisma.user_has_level.deleteMany({
+      where: {
+        id_user: id
+      }
+    })
+    const response = await prisma.user.delete({
+      where: {
+        id: id
+      }
+    })
+    res.status(200).send({message: "Usuário deletado com sucesso", response: response})
+  }catch(e){
+    res.status(400).send({error: e})
+  }
+}
+
+export async function updateUser(req, res) {
+  const { data } = req.body
+  const id = parseInt(req.params.id)
+  try{
+    const response = await prisma.user.update({
+      where: {
+        id: id
+      },
+      data: data
+    })
+    res.status(200).send({message: "Usuário atualizado com sucesso", response: response})
+  }catch(e){
+    res.status(400).send({error: e})
+  }
+}
+
+export async function getUserLevels(req,res) {
+  const user = parseInt(req.params.user)
+  
+  try{
+    const response = await prisma.user_has_level.findMany({
+      where: {
+        id_user: user
+      },
+      select: {
+        id_user: true,
+        id_level: true,
+        max_points: true,
+        level: {
+          select: {
+            id_world: true,
+            number: true,
+            label: true
+          }
+        }
+      },
+      orderBy: [
+        { level: { id_world: 'asc' } },
+        { level: { number: 'asc' } }
+      ]
+    })
+    return res.status(200).send({data: response})
+  }
+  catch(e){
+    return res.status(400).send({error: e})
+  }
+}
+
+export async function addlevelsToUser(req,res) {
+  const { levels } = req.body
+  const user = parseInt(req.params.user)
+  
+  try{
+    const data = []
+    levels.map((e)=>{
+      data.push({
+        id_level: e,
+        id_user: user
+      })
+    })
+    const response = await prisma.user_has_level.createMany({
+      data: data,
+      skipDuplicates: true
+    })
+    return res.status(200).send({data: response, message: 'Levels adicionados com sucesso'})
+  }
+  catch(e){
+    return res.status(400).send({error: e})
+  }
+}
+export async function removeLevelsFromUser(req,res) {
+  const { levels } = req.body
+  const user = parseInt(req.params.user)
+  
+  try{
+    const response = await prisma.user_has_level.deleteMany({
+      where: {
+        id_user: user,
+        id_level : {
+          in: levels
+        }
+      }
+    })
+    return res.status(200).send({data: response, message: 'Levels removidos com sucesso'})
+  }
+  catch(e){
+    return res.status(400).send({error: e})
+  }
+}
---
+++ backend/src/routes/user.routes.js
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import * as userController from '../controllers/userController.js'
+import { adminMiddleware, authMiddleware } from "../middlewares/authMiddleware.js";
+
+const router = Router();
+
+router.get("/me/", authMiddleware, userController.getMyUser);
+router.get("/", authMiddleware, adminMiddleware, userController.getUsers);
+router.post("/", authMiddleware, adminMiddleware, userController.createUser);
+router.patch("/:id", authMiddleware, adminMiddleware, userController.updateUser);
+router.delete("/:id", authMiddleware, adminMiddleware, userController.deleteUser);
+router.get("/levels/:user", authMiddleware, userController.getUserLevels);
+router.post("/levels/:user", authMiddleware, adminMiddleware, userController.addlevelsToUser);
+router.delete("/levels/:user", authMiddleware, adminMiddleware, userController.removeLevelsFromUser);
+
+
+
+export default router;
