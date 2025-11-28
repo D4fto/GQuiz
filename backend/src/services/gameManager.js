e0cde7b27480b6ad8331c3ecaed9ac23cd90f6c7
D4fto <163424876+D4fto@users.noreply.github.com>
2025-11-28 08:54:56 -0300

    Criação do gameManager
    Co-Authored-By: Eduardo <163414153+echeliga@users.noreply.github.com>
    Co-Authored-By: marceloAst <163414219+marceloAst@users.noreply.github.com>

---
+++ backend/src/services/gameManager.js
@@ -0,0 +1,49 @@
+import { levelGame } from "./games.js";
+export default class GameManager {
+    constructor() {
+        this.users = new Map(); // Store user sessions with userId as key
+        this.games = new Map(); // Store active games with gameId as key
+    }
+    getUser(userId) {
+        return this.users.get(userId);
+    }
+    setUserActualGame(userId, game) {
+        const user = this.users.get(userId);
+        if (!user) {
+            throw new Error('User not found.');
+        }
+        user.actualGame = game;
+        this.users.set(userId, user);
+    }
+    addUser(userId) {
+        if (this.users.has(userId)) {
+            throw new Error('User with this ID already exists.');
+        }
+        this.users.set(userId, { actualGame: null });
+    }
+    removeUser(userId) {
+        this.users.delete(userId)
+    }
+    getUserActualQuestion(userId){
+        const user = this.users.get(userId);
+        if (!user) {
+            throw new Error('User not found.');
+        }
+        if(!user.actualGame){
+            throw new Error('User is not in a game.');
+        }
+        return user.actualGame.getCurrentQuestion()
+
+    }
+    getUserScore(userId){
+        const user = this.users.get(userId);
+        if (!user) {
+            throw new Error('User not found.');
+        }
+        if(!user.actualGame){
+            throw new Error('User is not in a game.');
+        }
+        return user.actualGame.getUserScore()
+
+    }
+}
