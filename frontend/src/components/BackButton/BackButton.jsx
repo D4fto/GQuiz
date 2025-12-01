983198a1ee3891602c6a0e2a71fa84b1ca79bfc6
Eduardo <cheligaedu@gmail.com>
2025-12-01 09:17:33 -0300

    little correction in backbutton

--- frontend/src/components/BackButton/BackButton.jsx
+++ frontend/src/components/BackButton/BackButton.jsx
@@ -1,9 +1,10 @@
 import NextButton from '../NextButton/NextButton'
 import { useNavigate } from 'react-router-dom';
 import styles from './BackButton.module.css'
-export default function BackButton(){
+export default function BackButton({classes}){
     const navigate = useNavigate();
     return(
-        <NextButton classes={styles.mirror} onClick={()=>navigate(-1)}/>
+        <NextButton
+        classes={`${styles.mirror} ${classes}`} onClick={()=>navigate(-1)}/>
     );
 }
