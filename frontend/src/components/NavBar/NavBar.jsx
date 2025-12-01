4f9907fdca143dd414e16baa1e7742d4d7dd874f
D4fto <163424876+D4fto@users.noreply.github.com>
2025-12-01 08:07:17 -0300

    Atualização de imagem na navbar
    Co-Authored-By: marceloAst <163414219+marceloAst@users.noreply.github.com>

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -1,33 +1,36 @@
 import styles from './NavBar.module.css';
 import gql from '../../assets/gquiz-logo-white.svg';
-import perfil from '../../assets/perfil.svg';
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { useAuth } from '../../contexts/authContext';
 
 const noProfileRoutes = ['/question']
 
 export default function NavBar() {
+  const { user } = useAuth()
   const location = useLocation()
+  const navigate = useNavigate()
+  console.log(user)
 
 
   return (
-      <nav className={styles.navbar}>
-          <div className={styles.logo}>
-              <img src={gql} alt="GQuiz Logo" className={styles.logoImage} />
-          </div>
-          <div className={styles.routes}>
-            <Link to={'/'}>Home</Link>
-            <Link to={'/login'}>Login</Link>
-            <Link to={'/worlds'}>Worlds</Link>
-            <Link to={'/question'}>Question</Link>
-            <Link to={'/profile'}>Profile</Link>
-          </div>
-          {
-            !noProfileRoutes.includes(location.pathname) &&
-            <div className={styles.user}>
-              Ver Perfil
-              <img src={perfil} alt="Foto de perfil" className={styles.userImage}/>
-            </div>
-          }
-      </nav>
+    <nav className={styles.navbar}>
+      <div className={styles.logo}>
+          <img src={gql} alt="GQuiz Logo" className={styles.logoImage} />
+      </div>
+      <div className={styles.routes}>
+        <Link to={'/'}>Home</Link>
+        <Link to={'/login'}>Login</Link>
+        <Link to={'/worlds'}>Worlds</Link>
+        <Link to={'/question'}>Question</Link>
+        <Link to={'/profile'}>Profile</Link>
+      </div>
+      {
+        !noProfileRoutes.includes(location.pathname) &&
+        <div className={styles.user} onClick={()=>navigate('/profile')}>
+          Ver Perfil
+          <img src={import.meta.env.VITE_URL+"/"+user.imgName+'.png'} alt="Foto de perfil" className={styles.userImage}/>
+        </div>
+      }
+    </nav>
   );
 }
