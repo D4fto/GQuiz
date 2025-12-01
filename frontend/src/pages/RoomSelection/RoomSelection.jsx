cb5fbf27d75ebd1625b9e09ad347427f0bd03ad2
D4fto <163424876+D4fto@users.noreply.github.com>
2025-12-01 08:13:04 -0300

    Comunicação entre room-selection e backend
    Co-Authored-By: Eduardo <163414153+echeliga@users.noreply.github.com>

--- frontend/src/pages/RoomSelection/RoomSelection.jsx
+++ frontend/src/pages/RoomSelection/RoomSelection.jsx
@@ -1,26 +1,52 @@
-import React, { useState } from 'react';
+import { useState, useEffect } from 'react';
 import styles from './RoomSelection.module.css';
 import StyleSquare from '../../components/StyleSquare/StyleSquare';
 import BackButton from '../../components/BackButton/BackButton';
 import TitleBox from '../../components/TitleBox/TitleBox'
+import { useGame } from '../../contexts/gameContext';
 
 export default function RoomSelect() {
   const [searchTerm, setSearchTerm] = useState('');
+  const [rooms, setRooms] = useState([])
+  const [roomSelected, setRoomSelected] = useState(null)
+  const { joinRoom } = useGame()
   
-  const rooms = [
-    { id: 1, name: 'Sala1', people: '10/10', locked: true },
-    { id: 2, name: 'Sala2', people: '10/20', locked: false },
-    { id: 3, name: 'Sala3', people: '10/10', locked: false },
-    { id: 4, name: 'Sala4', people: '5/10', locked: true },
-    { id: 5, name: 'Sala5', people: '9/10', locked: true },
-    { id: 6, name: 'Sala6', people: '15/20', locked: false },
-    { id: 7, name: 'Sala7', people: '7/10', locked: false },
-    { id: 8, name: 'Sala8', people: '2/10', locked: false },
-    { id: 10, name: 'Sala10', people: '2/10', locked: false },
-  ];
+  async function updateRooms() {
+    try{
+      const response = await (await fetch(import.meta.env.VITE_API_URL+'/game/rooms',{
+        credentials: "include"
+      })).json()
+      response.rooms = response.rooms.filter((room)=>{
+        return room.acceptingPlayers
+      })
+      setRooms(response.rooms)
+    }catch(e){
+      console.error(e)
+    }
+  }
+
+  useEffect(()=>{
+    updateRooms()
+  },[])
+
+  function handleStart(){
+    joinRoom(roomSelected)
+  }
+  
+  // const rooms = [
+  //   { id: 1, name: 'Sala1', people: '10/10', locked: true },
+  //   { id: 2, name: 'Sala2', people: '10/20', locked: false },
+  //   { id: 3, name: 'Sala3', people: '10/10', locked: false },
+  //   { id: 4, name: 'Sala4', people: '5/10', locked: true },
+  //   { id: 5, name: 'Sala5', people: '9/10', locked: true },
+  //   { id: 6, name: 'Sala6', people: '15/20', locked: false },
+  //   { id: 7, name: 'Sala7', people: '7/10', locked: false },
+  //   { id: 8, name: 'Sala8', people: '2/10', locked: false },
+  //   { id: 10, name: 'Sala10', people: '2/10', locked: false },
+  // ];
 
   const filteredRooms = rooms.filter(room =>
-    room.name.toLowerCase().includes(searchTerm.toLowerCase())
+    room.id.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
   return (
@@ -42,13 +68,13 @@ export default function RoomSelect() {
               className={styles.input}
             />
           </div>
-          <button className={styles.addButton}>+</button>
+          <button className={styles.addButton} onClick={updateRooms}>+</button>
         </div>
 
         {/* Rooms List */}
         <div className={styles.roomsList}>
           {filteredRooms.map((room) => (
-            <div key={room.id} className={styles.roomItem}>
+            <div key={room.id} className={`${styles.roomItem} ${roomSelected==room.id?styles.selected:''}`} onClick={()=>setRoomSelected(room.id)}>
               <div className={styles.roomInfo}>
                 <svg className={styles.icon} viewBox="0 0 24 24" fill="none" stroke="currentColor">
                   <path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path>
@@ -56,12 +82,12 @@ export default function RoomSelect() {
                   <path d="M23 21v-2a4 4 0 0 0-3-3.87"></path>
                   <path d="M16 3.13a4 4 0 0 1 0 7.75"></path>
                 </svg>
-                <span className={styles.roomName}>{room.name}</span>
+                <span className={styles.roomName}>{room.id}</span>
               </div>
               
               <div className={styles.roomDetails}>
-                <span className={styles.roomScore}>{room.score}</span>
-                {room.locked && (
+                <span className={styles.roomScore}>{room.numberOfPlayers}/{room.maxNumberOfPlayers}</span>
+                {room.hasPassword && (
                   <svg className={styles.lockIcon} viewBox="0 0 24 24" fill="none" stroke="currentColor">
                     <rect x="3" y="11" width="18" height="11" rx="2" ry="2"></rect>
                     <path d="M7 11V7a5 5 0 0 1 10 0v4"></path>
@@ -74,7 +100,7 @@ export default function RoomSelect() {
 
         {/* Play Button */}
         <div className={styles.playButtonContainer}>
-          <button className={styles.playButton}>Jogar</button>
+          <button className={styles.playButton} onClick={handleStart}>Jogar</button>
         </div>
       </div>
         <BackButton/>
