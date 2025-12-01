ff5df637f225a5f357487aa92fb7050429bdbab5
marceloAst <marcelo.2007@alunos.utfpr.edu.br>
2025-12-01 10:05:51 -0300

    randomQuiz frontende

--- frontend/src/pages/randomQuiz/randomQuiz.jsx
+++ frontend/src/pages/randomQuiz/randomQuiz.jsx
@@ -1,10 +1,19 @@
 import RandomQuiz from "../../components/RandomQuiz/RandomQuiz";
+import BackButton from "../../components/BackButton/BackButton";
+import StyleSquare from "../../components/StyleSquare/StyleSquare";
 import styles from "./randomQuiz.module.css";
 
 export default function RandomQuizPage() {
   return (
-    <div className={styles.page}>
-      <RandomQuiz />
+    <div className={styles.container}>
+      <div className={styles.page}>
+        <RandomQuiz />
+
+
+
+        <BackButton classes={styles.boto} />
+      </div>
+      <div><StyleSquare option={2} innerColor={'var(--primary-dark)'} outColor={'var(--primary)'} position={'bottomRight'}></StyleSquare></div>
     </div>
   );
 }
