36d9e997f1fa043942337400c7a21fa2df3fa8f9
D4fto <163424876+D4fto@users.noreply.github.com>
2025-12-04 21:54:46 -0300

    Atualizando o ranking

--- frontend/src/components/GeneralRank/GeneralRank.jsx
+++ frontend/src/components/GeneralRank/GeneralRank.jsx
@@ -1,29 +1,43 @@
 import styles from './GeneralRank.module.css'
+import { useEffect, useState } from 'react'
 
-const data = [
-  {
-    name: 'Ronaldo',
-    points: 5504,
-    imgUrl: 'https://letsenhance.io/static/73136da51c245e80edc6ccfe44888a99/396e9/MainBefore.jpg'
-  },
-  {
-    name: 'Max JR',
-    points: 4675,
-    imgUrl: 'https://ufsb.edu.br/residenciapedagogica/images/phocagallery/galeria2/thumbs/phoca_thumb_l_image03_grd.png'
-  },
-  {
-    name: 'Andreia',
-    points: 1245,
-    imgUrl: 'https://t.ctcdn.com.br/JlHwiRHyv0mTD7GfRkIlgO6eQX8=/640x360/smart/i257652.jpeg'
-  },
-]
+// const data = [
+//   {
+//     name: 'Ronaldo',
+//     points: 5504,
+//     imgUrl: 'https://letsenhance.io/static/73136da51c245e80edc6ccfe44888a99/396e9/MainBefore.jpg'
+//   },
+//   {
+//     name: 'Max JR',
+//     points: 4675,
+//     imgUrl: 'https://ufsb.edu.br/residenciapedagogica/images/phocagallery/galeria2/thumbs/phoca_thumb_l_image03_grd.png'
+//   },
+//   {
+//     name: 'Andreia',
+//     points: 1245,
+//     imgUrl: 'https://t.ctcdn.com.br/JlHwiRHyv0mTD7GfRkIlgO6eQX8=/640x360/smart/i257652.jpeg'
+//   },
+// ]
 
-export default function GeneralRank(){
+export default function GeneralRank({length=15}){
+  const [data, setData] = useState([])
+
+  async function fetchData() {
+    const response = await (await fetch(import.meta.env.VITE_API_URL+'/ranking/'+length, {
+      credentials: 'include'
+    })).json()
+    setData(response.response)
+
+  }
+
+  useEffect(()=>{
+    fetchData()
+  },[])
   return <ul>
     {data.map((x,i)=>{
       return <li key={i}>
-        <img src={x.imgUrl} alt={"Imagem de " + x.name} className={styles.userImg}/>
-        <span>{x.name}</span>
+        <img src={import.meta.env.VITE_URL+'/geral.png'} alt={"Imagem de " + x.username} className={styles.userImg}/>
+        <span>{x.username}</span>
         <span>{x.points}pt</span>
       </li>
     })}
